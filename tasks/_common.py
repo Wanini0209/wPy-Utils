c8c5ae1402b5c6c5ff110723c181bd6b1605c4d7
Ting-Hsu Chang <Wanini.KLP.Mashimaro@gmail.com>
2022-09-29 16:20:52 +0800

    chore: fix wrong package name in `test-cov`

--- tasks/_common.py
+++ tasks/_common.py
@@ -5,7 +5,7 @@ import sys
 VENV_PREFIX = 'pipenv run'
 _COMMON_TARGETS = ['wutils', 'tests', 'setup.py', 'tasks']
 COMMON_TARGETS_AS_STR = ' '.join(_COMMON_TARGETS)
-TEST_TARGET = "{info.package}"
+TEST_TARGET = "wutils"
 
 # The differences between Linux and Windows
 USE_PTY = sys.platform != 'win32'
